41946ab7f89382b0a286efa063a0dfe00ca91822
BrunaBoscoADS3C <166921460+BrunaBoscoADS3C@users.noreply.github.com>
2024-11-04 14:53:24 -0300

    Atividade10

---
+++ Collection/AtividadeSet.java
@@ -0,0 +1,35 @@
+package Collection;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class AtividadeSet {
+
+	public static void main(String[] args) {
+		
+        Set<Integer> numeros = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (numeros.size() < 10) {
+            System.out.print("Digite um número inteiro: ");
+            int numero = scanner.nextInt();
+
+            if (!numeros.add(numero)) {
+                System.out.println("Número já inserido. Digite um número diferente.");
+            }
+        }
+
+        System.out.println("\nElementos da Collection Set:");
+        Iterator<Integer> iterator = numeros.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        scanner.close();
+	}
+}
+  
+		
+		 
